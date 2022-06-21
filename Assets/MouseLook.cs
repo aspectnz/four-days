b5866e1cd3bcf228340af60d38238812fd50c7bc
Shan A <slekupvimplyrataqq@protonmail.com>
2022-06-22 08:43:17 +1200

    Update MouseLook.cs

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +18,13 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
+        float mouseX =
+            Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY =
+            Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }

d172357bad2e5c611db2035d97244e32655ad93c
Shan A <slekupvimplyrataqq@protonmail.com>
2022-06-22 09:00:51 +1200

    player movement

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -13,6 +13,7 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -24,6 +25,7 @@ public class MouseLook : MonoBehaviour
             Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);

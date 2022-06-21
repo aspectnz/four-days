ef9b2d3a81812ee9fe740cccfed5eb332998e9de
Shan A <slekupvimplyrataqq@protonmail.com>
2022-06-22 09:01:20 +1200

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -27,6 +27,11 @@ public class PlayerMovement : MonoBehaviour
             Physics
                 .CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 

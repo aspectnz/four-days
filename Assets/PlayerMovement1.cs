3e0792a44a63eb22ccf2313fe420848cf0f3393b
Shan A <slekupvimplyrataqq@protonmail.com>
2022-06-22 17:16:30 +1200

    jumping

--- Assets/PlayerMovement1.cs
+++ Assets/PlayerMovement1.cs
@@ -6,7 +6,26 @@ public class PlayerMovement1 : MonoBehaviour
 {
 
   [Header("Movement")]
-  public float movespeed;
+  public float moveSpeed;
+
+  public float groundDrag;
+
+  public float jumpForce;
+  public float jumpCooldown;
+  public float airMultiplier;
+  bool readyToJump;
+
+  [Header("Keybinds")]
+  public KeyCode jumpKey = KeyCode.space;
+
+
+  [Header("Ground Check")]
+  public Transform groundCheck;
+  public float groundDistance = 0.4f;
+  public LayerMask whatIsGround;
+  bool grounded;
+
+
 
   public Transform orientation;
 
@@ -28,7 +47,16 @@ public class PlayerMovement1 : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    // Ground check 
+    grounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
+
     MyInput();
+    SpeedControl();
+
+    // Handle drag
+    if (grounded)
+      rb.drag = groundDrag;
+    else rb.drag = 0;
   }
 
   void FixedUpdate()
@@ -40,12 +68,55 @@ public class PlayerMovement1 : MonoBehaviour
   {
     horizontalInput = Input.GetAxisRaw("Horizontal");
     verticalInput = Input.GetAxisRaw("Vertical");
+
+    // When to jump
+    if (MyInput.GetKey(jumpKey) && readyToJump && grounded)
+    {
+      readyToJump = false;
+
+      Jump();
+
+      Invoke(nameof(ResetJump), jumpCooldown);
+    }
   }
 
   void MovePlayer()
   {
     // Calculate movement direction
     moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-    rb.AddForce(moveDirection.normalized * movespeed * 10f, ForceMode.Force);
+
+    // On ground
+    if (grounded)
+      rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+    // In air
+    else if (!grounded)
+      rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+  }
+
+  void SpeedControl()
+  {
+    Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+    // Limit velocity if needed
+    if (flatVel.magnitude > moveSpeed)
+    {
+      Vector3 limitedVel = flatVel.normalized * moveSpeed;
+      rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+
+    }
+  }
+
+  void Jump()
+  {
+    // Reset y velocity
+    rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+    rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+  }
+
+  void ResetJump()
+  {
+    readyToJump = true;
   }
 }
